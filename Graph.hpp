5a12b0ccefe6b1ad988d7ca4a946c3fc811315c8
RaulCruzRamirez <71723606+RaulCruzRamirez@users.noreply.github.com>
2025-11-06 05:11:26 +0000

    Functions: Done

--- Graph.hpp
+++ Graph.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <stdexcept>
+using namespace std;
 
 class Graph {
 private:
@@ -33,16 +35,27 @@ public:
     // Implementar!! 
     // Devuelve la cantidad de aristas
     int numEdges() const {
-        return 0;
+        int edgeNum = 0;
+        for (int i = 0; i < numVertices; ++i) {
+            for (int j = 0; j < numVertices; ++j) {
+                if(adjMatrix[i][j]!=0){edgeNum += adjMatrix[i][j];}
+            }
+        }
+        return edgeNum;
     }
 
     // Completa esta función
     // Devuelve el in-degree de un vertice
     int inDegree(int u) const {
+        int degreeNum = 0;
         if (u < 0 || u >= numVertices)
             throw std::out_of_range("Vertice fuera de rango");
         else {
+            for (int j = 0; j < numVertices; ++j) {
+                if(adjMatrix[j][u]!=-1){degreeNum += adjMatrix[j][u];}//std::cout << adjMatrix[j][2] << " ";}
+            }
         }
+        return degreeNum;
     }
 
     // Completa esta función
@@ -50,6 +63,24 @@ public:
     // En caso de que haya varios nodos que tengan el mayor inDegree,
     // devuelve true si u es uno de ellos
     bool isInfluencer(int u) const  {
+        int inDegreeAmount = -1, currAmount = -1;
+        stack<int> topNums;
+        for (int j = 0; j < numVertices; ++j) {
+            currAmount = inDegree(j);
+            if(currAmount>inDegreeAmount){
+                while(!topNums.empty()){
+                    topNums.pop();
+                }
+                topNums.push(j);
+                inDegreeAmount = currAmount;
+            }
+            else if(currAmount==inDegreeAmount){topNums.push(j);}
+        }
+        for(int i = 0; i < topNums.size();){
+            if(topNums.top()==u){return true;}
+            else{topNums.pop();}
+        }
+        return false;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ int main() {
   Graph G(10);
   G.addEdge(0,9);
   G.addEdge(4, 2);
+  G.addEdge(0, 2);
+  G.addEdge(2, 4);
+  G.addEdge(9, 1);
+  G.addEdge(9, 9);
+
+  cout << G.numEdges() << endl << endl;
+
+  cout << G.inDegree(9) << endl;
+  cout << G.inDegree(2) << endl;
+  cout << G.inDegree(4) << endl;
+  cout << G.inDegree(1) << endl;
+  cout << G.inDegree(0) << endl << endl;
+
+  cout << G.isInfluencer(9) << endl;
+  cout << G.isInfluencer(2) << endl;
+  cout << G.isInfluencer(4) << endl;
+  cout << G.isInfluencer(1) << endl << endl;
 
   G.print();
 }
